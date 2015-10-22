4d002a413a4481215b2f60cb10ffff5b6ca723d7
kaslemr <mattkasle@gmail.com>
2015-10-21 22:20:14 -0400

    Added likes, reblogs, trending among others

--- django_social/Tumblr/models.py
+++ django_social/Tumblr/models.py
@@ -2,22 +2,26 @@ from django.db import models
 
 # Create your models here.
 
-class Profile(models.Model):
+class User(models.Model):
     username = models.CharField(max_length=20)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     following = models.ManyToManyField("self", symmetrical=False)
-    
+
 
 class Entry(models.Model):
     id = models.ForeignKey(Profile)
     time_signature = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
+    content_text = models.TextField()
     content_link = models.URLField()
     content_video = models.URLField()
     content_photo = models.ImageField()
+    content_audio = models.URLField()
+    content_quote = model.TextField()
+    content_chat = model.TextField()
+    re_blog = model.ManyToManyField("self", symmetrical=False)
 
-class HomeFeed(models.Model):
+class Home_Page(models.Model):
     user = models.ForeignKey(Profile)
     contributors = models.ForeignKey(Profile.following)
     posts = models.ForeignKey(Entry, id=Profile.following)
@@ -25,4 +29,13 @@ class HomeFeed(models.Model):
 class Personal_Blog(models.Model):
     user = models.ForeignKey(Profile)
     posts = models.ForeignKey(Entry)
+    likes = models.ManyToManyField(Entry)
     followers = models.ForeignKey(Profile.following, id='self')
+
+class Explore(models.Model):
+    posts = model.ForeignKey(Entry)
+    trending_searches = model.ForeignKey(Trending)
+
+class Trending(models.Model):
+    posts = model.ForeignKey(Entry, id=most_liked)
+
