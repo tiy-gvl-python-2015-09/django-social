e2cb1ba571b687192b90334257609c38adccd861
andbar <barringera@gmail.com>
2015-10-22 09:38:17 -0400

    removed password

--- django_social/django_socialapp/models.py
+++ django_social/django_socialapp/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    password = models.CharField(max_length=50)
     photo = models.ImageField()
     connections = models.ManyToManyField("self")
     skills = models.TextField()
