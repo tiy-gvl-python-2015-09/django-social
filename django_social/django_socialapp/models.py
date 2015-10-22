4efcc1aacc5f8f297d56c4d4bdc269b1f750f8ad
andbar <barringera@gmail.com>
2015-10-22 00:39:44 -0400

    Updated models

--- django_social/django_socialapp/models.py
+++ django_social/django_socialapp/models.py
@@ -3,20 +3,11 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField()
     password = models.CharField(max_length=50)
     photo = models.ImageField()
     connections = models.ManyToManyField(self)
     skills = models.TextField()
-
-
-class Experience(models.Model):
-    user = models.ForeignKey(Person)
-    company = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    dates = models.DateField()
-    description = models.TextField()
 
 
 class Company(models.Model):
@@ -27,14 +18,12 @@ class Company(models.Model):
     followers = models.ManyToManyField(Person)
 
 
-class Education(models.Model):
+class Experience(models.Model):
     user = models.ForeignKey(Person)
-    school = models.CharField(max_length=100)
+    company = models.ForeignKey(Company)
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     dates = models.DateField()
-    degree = models.CharField(max_length=30)
-    subject = models.CharField(max_length=100)
-    grade = models.CharField(max_length=100)
-    activities = models.TextField()
     description = models.TextField()
 
 
@@ -42,3 +31,15 @@ class School(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     followers = models.ManyToManyField(Person)
+
+
+class Education(models.Model):
+    user = models.ForeignKey(Person)
+    school = models.ForeignKey(School)
+    dates = models.DateField()
+    degree = models.CharField(max_length=30)
+    subject = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+    activities = models.TextField()
+    description = models.TextField()
+
